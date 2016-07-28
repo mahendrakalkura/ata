200cc7d5d1ee4a2791fd40bad422c197ad6d3d6a
Mahendra Kalkura <mahendra@kalkura.com>
2016-07-28 19:02:53 +0530

    man

---
+++ manage.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from os import environ
+from sys import argv
+
+from django.core.management import execute_from_command_line
+
+if __name__ == '__main__':
+    environ.setdefault('DJANGO_SETTINGS_MODULE', 'ata.settings')
+    execute_from_command_line(argv)
